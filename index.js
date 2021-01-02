038737eda44a6a65dfd10a5cbcf1f350601b1298
antrikshmisri <antrikshmisri@gmail.com>
2021-01-02 14:00:23 +0530

    feat: added root files

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+app.set('view engine' , 'ejs')
+app.set('views' , path.join(__dirname , 'views'))
+
+app.get('/' , (req , res) =>{
+    res.render('home')
+})
+app.listen(5050 , () =>{
+    console.log('Listening on Port 5050')
+})
+
