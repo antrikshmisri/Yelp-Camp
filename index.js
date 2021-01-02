30aef88d37d9210dcf705881f1bdbd6a698e1063
antrikshmisri <antrikshmisri@gmail.com>
2021-01-02 15:29:45 +0530

    feat: added db connect logic in server

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+
+const db = mongoose.connection;
+db.on("error" , console.error.bind(console , "Connection error: "));
+db.once("open" , ()=>{
+    console.log("connection established")
+})
+
 app.set('view engine' , 'ejs')
 app.set('views' , path.join(__dirname , 'views'))
 
